49b5f11c888f00130f6b2a1cdc0b2666c9b4f434
voronnate <voronnate@yandex.ru>
2020-10-01 13:45:47 +0300

    New localizations

---
+++ localizations/en.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+  'localeUnix' => 'en_US',
+  'localeWindows' => 'english-american',
+  'tz' => 'Europe/London',
+
+  'Id' => 'ID',
+  'Id – reverse' => 'ID – reverse',
+  'Language (%s):' => 'Language:',
+);
